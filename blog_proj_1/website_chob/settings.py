79f9189f98938666598a9a6bf7b0d2245d0569ac
Charlie He <Charlie.y.he@gmail.com>
2021-01-08 19:28:44 -0800

    removed staticfiles storage

--- blog_proj_1/website_chob/settings.py
+++ blog_proj_1/website_chob/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
